46ae7bca9e6042709a63f2e79491464f7f50544f
avinh <avinhahuynh@gmail.com>
2023-02-16 03:25:51 -0800

    changed cookie to https://github.io

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
@@ -42,7 +42,7 @@ public class JwtApiController {
 			.path("/")
 			.maxAge(3600)
 			.sameSite("None; Secure")
-			.domain("locam.nighthawkcodingsociety.com")
+			.domain("https://slimeyturtles.github.io")
 			.build();
 		return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, tokenCookie.toString()).build();
 	}
