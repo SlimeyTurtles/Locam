85008e6961d0c9a9624f0d34962ba661bc58ee5c
avinh <avinhahuynh@gmail.com>
2023-02-16 01:22:34 -0800

    updated urls and pom xml

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -24,7 +24,7 @@ public class MvcConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://locam.nighthawkcoders.com", "https://slimeyturtles.github.io", "http://localhost:8085");
+        registry.addMapping("/**").allowedOrigins("https://slimeyturtles.github.io");
     }
     
 }
