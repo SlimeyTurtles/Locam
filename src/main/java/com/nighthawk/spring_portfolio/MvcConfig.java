901c755faa2fcebe0bf3856fe2efd56f9277c28d
avinh <avinhahuynh@gmail.com>
2023-02-16 02:20:49 -0800

    AAGAGG

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -24,7 +24,7 @@ public class MvcConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://slimeyturtles.github.io");
+        registry.addMapping("/**").allowedOrigins("https://locam.nighthawkcoders.com", "https://slimeyturtles.github.io", "http://localhost:8085");
     }
     
 }
